0667dc6e14452dc0282b49a50440e3c917d4d2ff
jsonfelix <wbqlove1234@gmail.com>
2022-06-10 13:04:00 +0800

    update bits check

--- mapclients/eth/test/TestLightNode.js
+++ mapclients/eth/test/TestLightNode.js
@@ -133,16 +133,33 @@ describe('WeightedMultiSig', function () {
 
 
 
-    it("should check updateBlockHeader", async () => {
-        const bits = '0x0d'; // 00000111
-        const message = "0x83c6030e8eb07e62c2f66a2afebcdcf0a0687ee96f8bfea7482a6ccc502e252502";
-        const sign = await bc.decodeG1("0x04017fa35d23482fb010423283569b741e611cfb967ee12673805bdecff8919a1c590bd715dbc1f6a8e133983225b571b8db7c349a2405cb15a266bcf1ed09b2");
+    it("should check updateBlockHeader none", async () => {
+        const bits = '0x00'; // 00000111
+        const _pairKeysAdd = [];
+        const _weights = [];
+        const epoch = 1;
+        //upateValidators(G1[] memory _pairKeysAdd, uint[] memory _weights, uint256 epoch, bytes memory bits)
+        assert(await wms.callStatic.upateValidators(_pairKeysAdd,_weights,epoch,bits));
+    });
 
-        const aggpk = ["0x1e765f27b1bc2822f1543fec2a14530db0eb56175e2cd5bc7c6567ef7d605204",
-            "0x10ebefa20bce22d0bce4bba43a151c8eedfed5c6487e98e21907da5384aba903",
-            "0x19933df8221f1532677d57c1bafb9220fa098a4774a482b37c98746d32d25ed3",
-            "0x1e9dcff0a16587d79dd67fc7917cdce9c2813b4e47adc7a1803e121e7cff9995"]
-        const epoch = 0;
-        assert(await wms.callStatic.checkSig(bits, message, sign, aggpk, epoch));
+    it("should check updateBlockHeader add", async () => {
+        const bits = '0x10'; // 00000111
+        let g4hex ="0x05fde1416ab5b30e4b140ad4a29a52cd9bc85ca27bd4662ba842a2e22118bea60dc32694f317d886daac5419b39412a33ee89e07d39d557e4e2b0e48696ac312";
+        const g4 = await bc.decodeG1(g4hex);
+        const _pairKeysAdd = [g4];
+        const _weights = [1];
+        const epoch = 2;
+        //upateValidators(G1[] memory _pairKeysAdd, uint[] memory _weights, uint256 epoch, bytes memory bits)
+        assert(await wms.callStatic.upateValidators(_pairKeysAdd,_weights,epoch,bits));
+    });
+
+
+    it("should check updateBlockHeader remove", async () => {
+        const bits = '0x00'; // 0x10000
+        const _pairKeysAdd = []
+        const _weights = [];
+        const epoch = 3;
+        //upateValidators(G1[] memory _pairKeysAdd, uint[] memory _weights, uint256 epoch, bytes memory bits)
+        assert(await wms.callStatic.upateValidators(_pairKeysAdd,_weights,epoch,bits));
     });
 });
