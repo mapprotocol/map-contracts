07a4bfddc5617ebe4d78ca61d61f322a4c7be52f
EdwardDaniels <EdwardDaniels0622@outlook.com>
2022-08-31 15:02:21 +0800

    "Update mcs test"

--- mcs/evm/deploy/MAPCrossChainServiceRelay.js
+++ mcs/evm/deploy/MAPCrossChainServiceRelay.js
@@ -1,6 +1,12 @@
 const BigNumber = require('bignumber.js')
 const {address} = require("hardhat/internal/core/config/config-validation");
 BigNumber.config({ROUNDING_MODE: BigNumber.ROUND_FLOOR})
+
+let managerAddress = "0x9E514B5A4e48Bf8D67C95Ddd586228294b7A58A6";
+let ethUsdt = "0xcfC80bEdDb70F12af6dA768FC30e396889DfCe26";
+let nearUsdt = "0x6d63735f746f6b656e5f312e6d63732e6d61703030312e746573746e6574";
+let mapUsdt = "0xe6EEE0EF1bdE38aC4e577647868FED43a3d0e3A9"
+
 module.exports = async function ({ethers, deployments}) {
     const {deploy} = deployments
     const {deployer,wcoin,mapcoin} = await ethers.getNamedSigners()
@@ -39,7 +45,34 @@ module.exports = async function ({ethers, deployments}) {
     console.log(wcoin.address);
     console.log(mapcoin.address);
 
-    await mcssRelay.initialize(wcoin.address,mapcoin.address,"0x0000000000747856657269667941646472657373");
+    await (await mcssRelay.initialize(wcoin.address,mapcoin.address,managerAddress)).wait();
+
+    await (await mcssRelay.setFeeCenter(feeCenter.address)).wait();
+
+    await (await mcssRelay.setTokenRegister(tokenRegister.address)).wait();
+
+    await (await mcssRelay.setIdTable(1313161555,1)).wait();
+
+
+    //tokenRegister set regToken
+    await (await tokenRegister.regToken(34434,ethUsdt,mapUsdt)).wait();
+
+    await (await tokenRegister.regToken(1313161555,nearUsdt,mapUsdt)).wait();
+
+    await (await tokenRegister.regToken(212,mapUsdt,ethUsdt)).wait();
+
+    //feeCenter set setChainTokenGasFee
+    await (await feeCenter.setChainTokenGasFee(34434,mapUsdt,"10000000000000000","10000000000000000000",200)).wait();
+
+    //MAPCrossChainServiceRelay set setTokenOtherChainDecimals
+    await (await mcssRelay.setTokenOtherChainDecimals(mapUsdt,34434,18));
+    await (await mcssRelay.setTokenOtherChainDecimals(mapUsdt,212,18));
+    await (await mcssRelay.setTokenOtherChainDecimals(mapUsdt,1313161555,24));
+
+    //MAPCrossChainServiceRelay set setVaultBalance
+    await (await mcssRelay.setVaultBalance(34434,mapUsdt,"10000000000000000000000000000")).wait();
+    await (await mcssRelay.setVaultBalance(1313161555,mapUsdt,"10000000000000000000000000000")).wait();
+
 
 
 }
--- mcs/evm/deploy/MapCrossChainService.js
+++ mcs/evm/deploy/MapCrossChainService.js
@@ -1,14 +1,14 @@
 const BigNumber = require('bignumber.js')
 BigNumber.config({ROUNDING_MODE: BigNumber.ROUND_FLOOR})
 
+let lightNodeAddress = "0x80Be41aEBFdaDBD58a65aa549cB266dAFb6b8304";
+let mcsRelayAddress = "0x23b51D50782c42Ac2dcda362E5243795205a02a4";
+
 module.exports = async function ({ethers, deployments}) {
     const {deploy} = deployments
     const {deployer,wcoin,mapcoin} = await ethers.getNamedSigners()
 
     console.log("deployer address:",deployer.address);
-
-    let lightNodeAddress = "0x28EF57E733C6F67067078e14B3CE159543a8A3c8";
-
 
     await deploy('MapCrossChainService', {
         from: deployer.address,
@@ -22,8 +22,15 @@ module.exports = async function ({ethers, deployments}) {
     console.log("MapCrossChainService address:",mcss.address);
 
 
-    await mcss.initialize(wcoin.address,mapcoin.address,lightNodeAddress);
+    await (await mcss.initialize(wcoin.address,mapcoin.address,lightNodeAddress)).wait();
     console.log("MapCrossChainService initialize success");
+
+    await (await mcss.setBridge(mcsRelayAddress,212)).wait();
+
+    await (await mcss.setCanBridgeToken("0x0000000000000000000000000000000000000000",212,true)).wait();
+
+    await (await mcss.setCanBridgeToken("0x0000000000000000000000000000000000000000",1313161555,true)).wait();
+
 
 }
 
