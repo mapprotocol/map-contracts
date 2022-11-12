30894386307e4dab4dde4b342e037d1cdeecff93
phil <phil@maplabs.io>
2022-11-12 16:16:51 +0800

    Replace vault token owner to manager.

--- mcs/evmv2/contracts/token/VaultTokenV2.sol
+++ mcs/evmv2/contracts/token/VaultTokenV2.sol
@@ -6,12 +6,15 @@ import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 import "../interface/IVaultTokenV2.sol";
 import "../utils/TransferHelper.sol";
 
 
-contract VaultTokenV2 is IVaultTokenV2, Ownable,ERC20Burnable {
+contract VaultTokenV2 is IVaultTokenV2, AccessControlEnumerable,ERC20Burnable {
     using SafeMath for uint256;
+
+    bytes32 public constant MANAGER_ROLE = keccak256("MANAGER_ROLE");
 
     // chain_id => vault_value
     mapping(uint256 => int256) public vaultBalance;
@@ -24,13 +27,30 @@ contract VaultTokenV2 is IVaultTokenV2, Ownable,ERC20Burnable {
 
 
     /**
-     * @dev Grants `DEFAULT_ADMIN_ROLE`, `MINTER_ROLE` and `PAUSER_ROLE` to the
+     * @dev Grants `DEFAULT_ADMIN_ROLE`, `MANAGER_ROLE` to the
      * account that deploys the contract.
      *
      * See {ERC20-constructor}.
      */
     constructor(address _underlying, string memory _name, string memory _symbol) ERC20(_name, _symbol) {
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+
+        _setupRole(MANAGER_ROLE, _msgSender());
+
         underlying = _underlying;
+    }
+
+    modifier onlyManager(){
+        require(hasRole(MANAGER_ROLE, msg.sender), "Caller is not a manager");
+        _;
+    }
+
+    function addManager(address manager) external onlyRole(DEFAULT_ADMIN_ROLE){
+        _setupRole(MANAGER_ROLE, manager);
+    }
+
+    function removeManager(address manager) external onlyRole(DEFAULT_ADMIN_ROLE){
+        _revokeRole(MANAGER_ROLE,manager);
     }
 
     function getVaultTokenAmount(uint256 _amount) public view returns (uint){
@@ -54,7 +74,7 @@ contract VaultTokenV2 is IVaultTokenV2, Ownable,ERC20Burnable {
         return underlying;
     }
 
-    function deposit(uint256 _fromChain, uint256 _amount, address _to) external override onlyOwner {
+    function deposit(uint256 _fromChain, uint256 _amount, address _to) external override onlyManager {
         uint256 amount = getVaultTokenAmount(_amount);
         _mint(_to, amount);
         //_setVaultValue(_fromChain, _amount, 0, 0);
@@ -65,7 +85,7 @@ contract VaultTokenV2 is IVaultTokenV2, Ownable,ERC20Burnable {
         emit DepositVault(underlying, _to, _amount, amount);
     }
 
-    function withdraw(uint256 _toChain, uint256 _amount, address _to) external override onlyOwner {
+    function withdraw(uint256 _toChain, uint256 _amount, address _to) external override onlyManager {
         uint amount = getTokenAmount(_amount);
         _burn(_to, _amount);
         //_setVaultValue(0, 0, _toChain, amount);
@@ -76,7 +96,7 @@ contract VaultTokenV2 is IVaultTokenV2, Ownable,ERC20Burnable {
         emit WithdrawVault(underlying, _to, _amount, amount);
     }
 
-    function transferToken(uint256 _fromChain, uint256 _amount,  uint256 _toChain, uint256 _outAmount, uint256 _relayChain, uint256 _fee) external override onlyOwner {
+    function transferToken(uint256 _fromChain, uint256 _amount,  uint256 _toChain, uint256 _outAmount, uint256 _relayChain, uint256 _fee) external override onlyManager {
         vaultBalance[_fromChain] += int256(_amount);
         vaultBalance[_toChain] -= int256(_outAmount);
         vaultBalance[_relayChain] -= int256(_fee);
