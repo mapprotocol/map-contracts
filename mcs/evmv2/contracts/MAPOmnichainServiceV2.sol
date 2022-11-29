2c72bc5b3a00b10fedfbeb14a441d50053bbdba1
EdwardDaniels <EdwardDaniels0622@outlook.com>
2022-11-29 16:06:36 +0800

    "Optimized native token processing"

--- mcs/evmv2/contracts/MAPOmnichainServiceV2.sol
+++ mcs/evmv2/contracts/MAPOmnichainServiceV2.sol
@@ -46,9 +46,7 @@ contract MAPOmnichainServiceV2 is ReentrancyGuard, Initializable, Pausable, IMOS
     }
 
 
-    receive() external payable {
-        require(msg.sender == wToken, "only wToken");
-    }
+    receive() external payable {}
 
 
     modifier checkOrder(bytes32 _orderId) {
@@ -105,7 +103,9 @@ contract MAPOmnichainServiceV2 is ReentrancyGuard, Initializable, Pausable, IMOS
         if (_token == wToken) {
             TransferHelper.safeWithdraw(wToken, _amount);
             TransferHelper.safeTransferETH(_receiver, _amount);
-        } else {
+        } else if(_token == address(0)){
+            TransferHelper.safeTransferETH(_receiver, _amount);
+        }else {
             IERC20(_token).transfer(_receiver, _amount);
         }
     }
