0e48561d29da6ba102f5f5532bff4e3b0e99128a
EdwardDaniels <EdwardDaniels0622@outlook.com>
2022-12-26 17:25:47 +0800

    "timelock task"

--- mos/evmv2/tasks/index.js
+++ mos/evmv2/tasks/index.js
@@ -184,3 +184,24 @@ task("mosList",
 )
     .addOptionalParam("mos", "The mos address, default mos", "mos", types.string)
     .addOptionalParam("token", "The token address, default wtoken", "wtoken", types.string)
+
+task("customData",
+    "Construct multi-sign data",
+    require("./customData")
+)
+    .addParam("method", "The name of the method you want to propose")
+    .addParam("methodarg", "You want the method parameters for the proposal,multiple parameters are distinguished by commas ',' in sequence")
+    .addOptionalParam("safeaddress", "This is the gnosis safe multi-sign address","0x21624d0634c696f6c357cBd8c5B7f629aFf045f7",types.string)
+    .addOptionalParam("targetaddress", "Execute the target contract address","0xEd26be082C8145081085Ca58d01e8F2a633fBd03",types.string)
+    .addOptionalParam("valuenum", " value","0",types.string)
+    .addOptionalParam("delaynum", "How long is the delay","50",types.string)
+    .addOptionalParam("ctype", "The type of contract you want to execute(mos,relay,register)","relay",types.string)
+    .addOptionalParam("timelockaddress", "The time lock contract address","0x6559AfD04c08d8ebF6c45f0C750237D04f80a8A2",types.string)
+
+task("executeTimeLock",
+    "",
+    require("./executeTimeLock")
+
+)
+    .addParam("executeid","This is using Gnosis safe's nonce as credential")
+    .addOptionalParam("timelockaddress", "withdraw value","0x6559AfD04c08d8ebF6c45f0C750237D04f80a8A2",types.string)
