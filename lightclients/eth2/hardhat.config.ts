356ef408874dfa1fbb5ccac208f3a606f93a9c51
pandarr <panda007.2021@gmail.com>
2023-04-11 14:34:27 +0800

    update map dev network chain id

--- lightclients/eth2/hardhat.config.ts
+++ lightclients/eth2/hardhat.config.ts
@@ -29,7 +29,7 @@ const config: HardhatUserConfig = {
             chainId: 43112,
         },
         dev: {
-            chainId: 212,
+            chainId: 213,
             url: "http://3.0.19.66:7445",
             accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
         },
