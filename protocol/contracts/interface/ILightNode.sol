7d7bc42736a7ca87be66187287b493af6df65fb9
philllau <phil@maplabs.io>
2023-07-30 14:42:10 +0800

    0.3.0 Update lightnode interface and MPT lib.

--- protocol/contracts/interface/ILightNode.sol
+++ protocol/contracts/interface/ILightNode.sol
@@ -3,13 +3,19 @@
 pragma solidity ^0.8.0;
 
 interface ILightNode {
+
+    // Verify the validity of the transaction according to the header, receipt
+    // The interface will be updated later to return logs
     function verifyProofData(bytes memory _receiptProof) external view returns (bool success, string memory message, bytes memory logs);
 
     function updateBlockHeader(bytes memory _blockHeader) external;
 
     function updateLightClient(bytes memory _data) external;
 
+    // Get client state
     function clientState() external view returns(bytes memory);
+
+    function finalizedState(bytes memory _data) external view returns(bytes memory);
 
     function headerHeight() external view returns (uint256 height);
 
