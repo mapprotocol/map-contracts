b9e33a2487aa308579595516d31fe8ee17af3383
philllau <phil@maplabs.io>
2023-08-30 15:07:44 +0800

    protocol: Update ILightNode.

--- protocol/contracts/interface/ILightNode.sol
+++ protocol/contracts/interface/ILightNode.sol
@@ -4,13 +4,18 @@ pragma solidity ^0.8.0;
 
 interface ILightNode {
 
-    // Verify the validity of the transaction according to the header, receipt
-    // The interface will be updated later to return logs
-    function verifyProofData(bytes memory _receiptProof) external view returns (bool success, string memory message, bytes memory logs);
+    event UpdateBlockHeader(address indexed maintainer, uint256 indexed blockHeight);
+
 
     function updateBlockHeader(bytes memory _blockHeader) external;
 
     function updateLightClient(bytes memory _data) external;
+
+
+    // Verify the validity of the transaction according to the header, receipt
+    // The interface will be updated later to return logs
+    function verifyProofData(bytes memory _receiptProof) external view returns (bool success, string memory message, bytes memory logs);
+
 
     // Get client state
     function clientState() external view returns(bytes memory);
@@ -19,5 +24,6 @@ interface ILightNode {
 
     function headerHeight() external view returns (uint256 height);
 
+    //
     function verifiableHeaderRange() external view returns (uint256, uint256);
 }
