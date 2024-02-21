b2a24dc4cbe79d248db27f03aefc9914be4aabb3
EdwardDaniels <EdwardDaniels0622@outlook.com>
2024-02-21 13:34:06 +0800

    add miyoulab

---
+++ lightclients/miyou/test/lightNode.test.ts
@@ -0,0 +1,267 @@
+import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import { BigNumber } from "ethers";
+let data = require("../data/blocks.js");
+import {
+    BlockHeader, getBlock,
+    TxLog, ReceiptProof,
+    TxReceipt, index2key,
+    ProofData
+} from "../utils/Util"
+
+// //extraData =  EXTRA_VANITY + address... + EXTRASEAL 32 + 20*n + 65
+let minEpochBlockExtraDataLen = 117
+let chainId = 30088
+let confirms = 10
+
+describe("LightNode", function () {
+    // We define a fixture to reuse the same setup in every test.
+    // We use loadFixture to run this setup once, snapshot that state,
+    // and reset Hardhat Network to that snapshopt in every test.
+    async function deployFixture() {
+        let [wallet] = await ethers.getSigners();
+
+        const MPTVerify = await ethers.getContractFactory("MPTVerify");
+
+        const mPTVerify = await MPTVerify.deploy();
+
+        await mPTVerify.connect(wallet).deployed();
+
+        const LightNode = await ethers.getContractFactory("LightNode");
+
+        const lightNode = await LightNode.deploy();
+
+        await lightNode.connect(wallet).deployed();
+
+        const LightNodeProxy = await ethers.getContractFactory("LightNodeProxy");
+
+        let initBlock = data.initBlock;
+        let initData = LightNode.interface.encodeFunctionData("initialize", [chainId,minEpochBlockExtraDataLen, wallet.address, mPTVerify.address,confirms,initBlock]);
+	
+        const lightNodeProxy = await LightNodeProxy.deploy(lightNode.address, initData);
+
+        await lightNodeProxy.connect(wallet).deployed();
+
+        let proxy = LightNode.attach(lightNodeProxy.address);
+
+        return proxy;
+
+    }
+
+    describe("Deployment", function () {
+
+
+        it("initBlock() -> correct", async function () {
+
+
+            let [wallet, other] = await ethers.getSigners();
+
+            const lightNode = await loadFixture(deployFixture);
+
+            let current = await lightNode.headerHeight();
+
+            expect(current).to.eq(3720000)
+
+        });
+
+        it("upgradeTo() -> reverts only Admin", async function () {
+
+            let [wallet, other] = await ethers.getSigners();
+
+            let lightNode = await loadFixture(deployFixture);
+
+            let admin = await lightNode.getAdmin();
+
+            expect(admin).to.not.eq(other.address);
+
+            const LightNode = await ethers.getContractFactory("LightNode");
+            const newImplement = await LightNode.connect(wallet).deploy();
+            await newImplement.deployed();
+
+            await expect(lightNode.connect(other).upgradeTo(newImplement.address)).to.be.revertedWith('LightNode: only Admin can upgrade');
+
+        });
+
+        it("upgradeTo() -> correct", async function () {
+
+            let [wallet, other] = await ethers.getSigners();
+
+            let lightNode = await loadFixture(deployFixture);
+
+            let admin = await lightNode.getAdmin();
+
+            expect(admin).to.not.eq(other.address);
+
+            const LightNode = await ethers.getContractFactory("LightNode");
+            const newImplement = await LightNode.connect(wallet).deploy();
+            await newImplement.deployed();
+
+            let oldImplement = await lightNode.getImplementation();
+
+            expect(oldImplement).to.not.eq(newImplement.address);
+
+            await lightNode.connect(wallet).upgradeTo(newImplement.address);
+
+            expect(await lightNode.getImplementation()).to.eq(newImplement.address);
+
+        });
+
+
+        it("changeAdmin() -> reverts only Admin", async function () {
+
+            let [wallet, other] = await ethers.getSigners();
+
+            let lightNode = await loadFixture(deployFixture);
+
+            let admin = await lightNode.getAdmin();
+
+            expect(admin).to.eq(wallet.address);
+
+            await expect(lightNode.connect(other).setPendingAdmin(other.address)).to.be.revertedWith("lightnode :: only admin");
+
+        });
+
+        it("changeAdmin() -> reverts for zero address", async function () {
+
+            let [wallet, other] = await ethers.getSigners();
+
+            let lightNode = await loadFixture(deployFixture);
+
+            let admin = await lightNode.getAdmin();
+
+            expect(admin).to.eq(wallet.address);
+
+            await expect(lightNode.connect(wallet).setPendingAdmin(ethers.constants.AddressZero)).to.be.revertedWith("Ownable: pendingAdmin is the zero address");
+
+        });
+
+        it("changeAdmin() -> correct ", async function () {
+
+            let [wallet, other] = await ethers.getSigners();
+
+            let lightNode = await loadFixture(deployFixture);
+
+            let admin = await lightNode.getAdmin();
+
+            expect(admin).to.eq(wallet.address); 
+
+            await (await lightNode.connect(wallet).setPendingAdmin(other.address)).wait();
+
+            let pendingAdmin = await lightNode.pendingAdmin();
+
+            expect(pendingAdmin).eq(other.address);
+
+            await expect(lightNode.connect(wallet).changeAdmin()).to.be.revertedWith('only pendingAdmin');
+
+            await (await lightNode.connect(other).changeAdmin()).wait();
+
+
+            expect(await lightNode.getAdmin()).to.eq(other.address);
+
+
+        });
+
+
+        it("togglePause() -> reverts  only admin ", async function () {
+
+            let [wallet, other] = await ethers.getSigners();
+
+            let lightNode = await loadFixture(deployFixture);
+
+            let paused = await lightNode.paused();
+
+            expect(paused).to.false;
+
+            await expect(lightNode.connect(other).togglePause(true)).to.be.revertedWith("lightnode :: only admin");
+
+        });
+
+        it("togglePause() -> correct ", async function () {
+
+            let [wallet, other] = await ethers.getSigners();
+
+            let lightNode = await loadFixture(deployFixture);
+
+            let paused = await lightNode.paused();
+
+            expect(paused).to.false;
+
+            await lightNode.connect(wallet).togglePause(true);
+
+            expect(await lightNode.paused()).to.true;
+
+            await lightNode.connect(wallet).togglePause(false);
+
+            expect(await lightNode.paused()).to.false;
+
+        });
+
+
+        it("updateBlockHeader() -> reverts paused ", async function () {
+
+            let [wallet] = await ethers.getSigners();
+
+            let lightNode = await loadFixture(deployFixture);
+
+
+            await lightNode.connect(wallet).togglePause(true);
+
+            await expect(lightNode.updateBlockHeader(await lightNode.getHeadersBytes(data.addBlock_1s))).to.be.revertedWith('Pausable: paused');
+
+        });
+
+        it("updateBlockHeader() -> correct ", async function () {
+
+            let [wallet] = await ethers.getSigners();
+
+            let lightNode = await loadFixture(deployFixture);
+
+
+            await lightNode.updateBlockHeader(await lightNode.getHeadersBytes(data.addBlock_1s));
+
+            let current = await lightNode.headerHeight();
+
+            expect(current).to.eq(3750000)
+
+            await lightNode.updateBlockHeader(await lightNode.getHeadersBytes(data.addBlock_2s));
+
+            current = await lightNode.headerHeight();
+
+            expect(current).to.eq(3780000)
+
+        });
+
+
+        it("verifyProofData() -> correct ", async function () {
+
+            let [wallet] = await ethers.getSigners();
+
+            let lightNode = await loadFixture(deployFixture);
+
+
+            await lightNode.updateBlockHeader(await lightNode.getHeadersBytes(data.addBlock_1s));
+            let current = await lightNode.headerHeight();
+
+            expect(current).to.eq(3750000)
+
+            let receiptProof = new ReceiptProof(data.txReceipt, index2key(BigNumber.from(data.proof.key).toNumber(), data.proof.proof.length), data.proof.proof);
+
+            let proofData = new ProofData(data.proofHeaders, receiptProof);
+
+            let proofBytes = await lightNode.getBytes(proofData);
+
+            let result = await lightNode.verifyProofData(proofBytes, { gasLimit: 20000000 });
+
+            expect(result.success).to.true;
+        });
+
+
+
+    });
+
+
+
+
+});
---
+++ lightclients/miyou/utils/Util.ts
@@ -0,0 +1,272 @@
+import { BigNumber } from "ethers";
+import { JsonRpcProvider } from "@ethersproject/providers";
+const Rpc = require('isomorphic-rpc')
+const { encode, toBuffer } = require('eth-util-lite')
+import { ethers } from "hardhat";
+const Tree = require('merkle-patricia-tree')
+const { Header, Proof, Receipt, Transaction } = require('eth-object')
+const { promisfy } = require('promisfy')
+export class BlockHeader {
+
+    // struct BlockHeader {
+    //     bytes parentHash;
+    //     bytes sha3Uncles;
+    //     address miner;
+    //     bytes stateRoot;
+    //     bytes transactionsRoot;
+    //     bytes receiptsRoot;
+    //     bytes logsBloom;
+    //     uint256 difficulty;
+    //     uint256 number;
+    //     uint256 gasLimit;
+    //     uint256 gasUsed;
+    //     uint256 timestamp;
+    //     bytes extraData;
+    //     bytes mixHash;
+    //     bytes nonce;
+    // }
+    public parentHash?: string;
+    public sha3Uncles?: string;
+    public miner?: string;
+    public stateRoot?: string;
+    public transactionsRoot?: string
+    public receiptsRoot?: string
+    public logsBloom?: string
+    public difficulty?: BigNumber
+    public number?: BigNumber
+    public gasLimit?: BigNumber
+    public gasUsed?: BigNumber
+    public timestamp?: BigNumber
+    public extraData: string
+    public mixHash?: string
+    public nonce?: string
+
+    constructor(parentHash: string,
+        sha3Uncles: string,
+        miner: string, stateRoot: string,
+        transactionsRoot: string,
+        receiptsRoot: string,
+        logsBloom: string,
+        difficulty: BigNumber,
+        number: BigNumber,
+        gasLimit: BigNumber,
+        gasUsed: BigNumber,
+        timestamp: BigNumber,
+        extraData: string,
+        mixHash: string,
+        nonce: string
+    ) {
+
+        this.parentHash = parentHash;
+        this.sha3Uncles = sha3Uncles;
+        this.miner = miner;
+        this.stateRoot = stateRoot;
+        this.transactionsRoot = transactionsRoot;
+        this.receiptsRoot = receiptsRoot;
+        this.logsBloom = logsBloom;
+        this.difficulty = difficulty;
+        this.number = number,
+            this.gasLimit = gasLimit,
+            this.gasUsed = gasUsed;
+        this.timestamp = timestamp;
+        this.extraData = extraData;
+        this.mixHash = mixHash;
+        this.nonce = nonce;
+
+    }
+
+}
+// struct TxLog {
+//     address addr;
+//     bytes[] topics;
+//     bytes data;
+// }
+export class TxLog {
+    public addr?: string;
+    public topics?: Array<string>;
+    public data?: string
+
+    constructor(addr: string, topics: Array<string>, data: string) {
+        this.addr = addr;
+        this.topics = topics;
+        this.data = data;
+    }
+}
+
+// struct TxReceipt {
+//     uint256 receiptType;
+//     bytes postStateOrStatus;
+//     uint256 cumulativeGasUsed;
+//     bytes bloom;
+//     TxLog[] logs;
+// }
+export class TxReceipt {
+    public receiptType?: BigNumber;
+    public postStateOrStatus?: string;
+    public cumulativeGasUsed?: BigNumber;
+    public bloom?: string;
+    public logs?: Array<TxLog>
+
+    constructor(receiptType: BigNumber, postStateOrStatus: string, cumulativeGasUsed: BigNumber, bloom: string, logs: Array<TxLog>) {
+        this.receiptType = receiptType;
+        this.postStateOrStatus = postStateOrStatus;
+        this.cumulativeGasUsed = cumulativeGasUsed;
+        this.bloom = bloom;
+        this.logs = logs;
+    }
+}
+
+
+//   struct ReceiptProof {
+//     TxReceipt txReceipt;
+//     bytes keyIndex;
+//     bytes[] proof;
+// }
+
+export class ReceiptProof {
+    public txReceipt?: TxReceipt;
+    public keyIndex?: string;
+    public proof?: Array<string>;
+
+    constructor(txReceipt: TxReceipt, keyIndex: string, proof: Array<string>) {
+        this.txReceipt = txReceipt;
+        this.keyIndex = keyIndex;
+        this.proof = proof
+    }
+}
+
+
+
+
+export class ProofData {
+    public headers?: BlockHeader[];
+    public receiptProof?: ReceiptProof;
+
+
+    constructor(headers: BlockHeader[], receiptProof: ReceiptProof) {
+        this.headers = headers;
+        this.receiptProof = receiptProof;
+
+    }
+}
+
+export async function getBlock(blockNumber: number, provider: JsonRpcProvider) {
+
+    let block = await provider.getBlock(blockNumber);
+
+    const params: { [key: string]: any } = {
+        includeTransactions: !!false
+    };
+    params.blockHash = block.hash;
+
+    let rpcHeader = await provider.perform("getBlock", params);
+
+    let blockHeader = new BlockHeader(rpcHeader.parentHash, rpcHeader.sha3Uncles,
+        rpcHeader.miner, rpcHeader.stateRoot, rpcHeader.transactionsRoot,
+        rpcHeader.receiptsRoot, rpcHeader.logsBloom, BigNumber.from(rpcHeader.difficulty), BigNumber.from(rpcHeader.number),
+        BigNumber.from(rpcHeader.gasLimit), BigNumber.from(rpcHeader.gasUsed), BigNumber.from(rpcHeader.timestamp),
+        rpcHeader.extraData, rpcHeader.mixHash, rpcHeader.nonce);
+		console.log('["'+rpcHeader.parentHash+'","' +rpcHeader.sha3Uncles+'","' +
+        rpcHeader.miner+'","' + rpcHeader.stateRoot+'","' + rpcHeader.transactionsRoot+'","' +
+        rpcHeader.receiptsRoot+'","' + rpcHeader.logsBloom+'",' + BigNumber.from(rpcHeader.difficulty).toString() +',' + BigNumber.from(rpcHeader.number).toString()+',' +
+        BigNumber.from(rpcHeader.gasLimit).toString()+',' +  BigNumber.from(rpcHeader.gasUsed).toString()+',' + BigNumber.from(rpcHeader.timestamp).toString()+',"' + 
+        rpcHeader.extraData+'","' +  rpcHeader.mixHash+'","' +  rpcHeader.nonce +'"]')
+
+    return blockHeader;
+}
+
+export function index2key(index: number, proofLength: number) {
+    const actualkey: Array<number> = new Array<number>;
+    const encoded = buffer2hex(encode(index)).slice(2);
+    let key = [...new Array(encoded.length / 2).keys()].map(i => parseInt(encoded[i * 2] + encoded[i * 2 + 1], 16));
+
+    key.forEach(val => {
+        if (actualkey.length + 1 === proofLength) {
+            actualkey.push(val);
+        } else {
+            actualkey.push(Math.floor(val / 16));
+            actualkey.push(val % 16);
+        }
+    });
+    return '0x' + actualkey.map(v => v.toString(16).padStart(2, '0')).join('');
+}
+
+function buffer2hex(buffer: Buffer) {
+    return '0x' + buffer.toString('hex');
+}
+
+
+export async function getProof(txHash: string, rpc: string,confirms:number|string) {
+
+    const provider = new ethers.providers.JsonRpcProvider(rpc);
+    let r = await provider.getTransactionReceipt(txHash);
+    let block = await getBlock(r.blockNumber, provider);
+    let headers :Array<BlockHeader> = new Array<BlockHeader>();
+    for(let i = 0;i < confirms;i++){
+      let lastHeader = await getBlock(r.blockNumber + i, provider);
+        headers.push(lastHeader);
+    }
+
+    let logs: TxLog[] = new Array<TxLog>();
+
+    for (let i = 0; i < r.logs.length; i++) {
+
+        let log = new TxLog(r.logs[i].address, r.logs[i].topics, r.logs[i].data);
+		console.log(log)
+        logs.push(log);
+    }
+    let txReceipt = new TxReceipt(BigNumber.from(r.type), BigNumber.from(r.status || r.root).toHexString(), BigNumber.from(r.cumulativeGasUsed), r.logsBloom, logs);
+	
+    let proof = await getReceipt(txHash, rpc);
+    let receiptProof = new ReceiptProof(txReceipt, proof.key, proof.proof);
+    let proofData = new ProofData(headers, receiptProof);
+
+    return proofData;
+}
+
+async function getReceipt(txHash: string, uri?: string) {
+
+
+    const resp = await receiptProof(txHash, uri);
+
+    let proofs: Array<string> = new Array<string>();
+
+    for (let i = 0; i < resp.receiptProof.length; i++) {
+        proofs[i] = '0x' + encode(resp.receiptProof[i]).toString('hex');
+    }
+    return {
+        proof: proofs,
+        key: resp.txIndex // '0x12' => Nunmber
+    }
+
+
+}
+
+async function receiptProof(txHash: string, uri: string | undefined) {
+    let rpc = new Rpc(uri)
+    let targetReceipt = await rpc.eth_getTransactionReceipt(txHash)
+    if (!targetReceipt) { throw new Error("txhash/targetReceipt not found. (use Archive node)") }
+
+    let rpcBlock = await rpc.eth_getBlockByHash(targetReceipt.blockHash, false)
+
+    let receipts = await Promise.all(rpcBlock.transactions.map((siblingTxHash: string) => {
+        return rpc.eth_getTransactionReceipt(siblingTxHash)
+    }))
+
+    let tree = new Tree();
+    await Promise.all(receipts.map((siblingReceipt, index) => {
+        let siblingPath = encode(index)
+        let serializedReceipt = Receipt.fromRpc(siblingReceipt).serialize()
+        if (siblingReceipt.type && siblingReceipt.type != '0x0') {
+            serializedReceipt = Buffer.concat([Buffer.from([siblingReceipt.type]), serializedReceipt])
+        }
+        return promisfy(tree.put, tree)(siblingPath, serializedReceipt)
+    }))
+    let [_, __, stack] = await promisfy(tree.findPath, tree)(encode(targetReceipt.transactionIndex))
+    return {
+        header: Header.fromRpc(rpcBlock),
+        receiptProof: Proof.fromStack(stack),
+        txIndex: targetReceipt.transactionIndex,
+    }
+}
+
