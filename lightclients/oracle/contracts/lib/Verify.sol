b75e69bcdc7d66915d70ea5e05bf38f73fd8062b
Anthony4548 <yangdewu1990@163.com>
2024-04-22 16:23:17 +0800

    oracle light node rlp encode TxReceipt move to offChain

--- lightclients/oracle/contracts/lib/Verify.sol
+++ lightclients/oracle/contracts/lib/Verify.sol
@@ -1,35 +1,21 @@
+
 // SPDX-License-Identifier: MIT
 
 pragma solidity 0.8.20;
 
 import "@mapprotocol/protocol/contracts/lib/RLPReader.sol";
-import "@mapprotocol/protocol/contracts/lib/RLPEncode.sol";
 import "@mapprotocol/protocol/contracts/interface/IMPTVerify.sol";
 
 library Verify {
-    using RLPReader for bytes;
-    using RLPReader for uint256;
     using RLPReader for RLPReader.RLPItem;
     using RLPReader for RLPReader.Iterator;
+    using RLPReader for bytes;
 
     struct ReceiptProof {
-        TxReceipt txReceipt;
+        bytes txReceipt;
+        uint256 receiptType;
         bytes keyIndex;
         bytes[] proof;
-    }
-
-    struct TxReceipt {
-        uint256 receiptType;
-        bytes postStateOrStatus;
-        uint256 cumulativeGasUsed;
-        bytes bloom;
-        TxLog[] logs;
-    }
-
-    struct TxLog {
-        address addr;
-        bytes[] topics;
-        bytes data;
     }
 
     function _validateProof(
@@ -37,43 +23,17 @@ library Verify {
         ReceiptProof memory _receipt,
         address _mptVerify
     ) internal pure returns (bool success, bytes memory logs) {
-        bytes memory bytesReceipt = _encodeReceipt(_receipt.txReceipt);
+        bytes memory bytesReceipt = _receipt.txReceipt;
         bytes memory expectedValue = bytesReceipt;
-        if (_receipt.txReceipt.receiptType > 0) {
-            expectedValue = abi.encodePacked(bytes1(uint8(_receipt.txReceipt.receiptType)), bytesReceipt);
+        if (_receipt.receiptType > 0) {
+            expectedValue = abi.encodePacked(bytes1(uint8(_receipt.receiptType)), expectedValue);
         }
-
         success = IMPTVerify(_mptVerify).verifyTrieProof(
             _receiptsRoot,
             _receipt.keyIndex,
             _receipt.proof,
             expectedValue
         );
-
         if (success) logs = bytesReceipt.toRlpItem().toList()[3].toRlpBytes(); // list length must be 4
-    }
-
-    function _encodeReceipt(TxReceipt memory _txReceipt) internal pure returns (bytes memory output) {
-        bytes[] memory list = new bytes[](4);
-        list[0] = RLPEncode.encodeBytes(_txReceipt.postStateOrStatus);
-        list[1] = RLPEncode.encodeUint(_txReceipt.cumulativeGasUsed);
-        list[2] = RLPEncode.encodeBytes(_txReceipt.bloom);
-        bytes[] memory listLog = new bytes[](_txReceipt.logs.length);
-        bytes[] memory loglist = new bytes[](3);
-
-        for (uint256 j = 0; j < _txReceipt.logs.length; j++) {
-            loglist[0] = RLPEncode.encodeAddress(_txReceipt.logs[j].addr);
-            bytes[] memory loglist1 = new bytes[](_txReceipt.logs[j].topics.length);
-
-            for (uint256 i = 0; i < _txReceipt.logs[j].topics.length; i++) {
-                loglist1[i] = RLPEncode.encodeBytes(_txReceipt.logs[j].topics[i]);
-            }
-            loglist[1] = RLPEncode.encodeList(loglist1);
-            loglist[2] = RLPEncode.encodeBytes(_txReceipt.logs[j].data);
-            bytes memory logBytes = RLPEncode.encodeList(loglist);
-            listLog[j] = logBytes;
-        }
-        list[3] = RLPEncode.encodeList(listLog);
-        output = RLPEncode.encodeList(list);
     }
 }
